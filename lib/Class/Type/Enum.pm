989d4b53d72caf184d265cae56beea3dabe633b8
Meredith Howard <mhoward@roomag.org>
2015-11-09 05:11:38 -0500

    Require List::Util min 1.33

--- lib/Class/Type/Enum.pm
+++ lib/Class/Type/Enum.pm
@@ -71,7 +71,7 @@ use strict;
 use warnings;
 
 use Function::Parameters ':strict';
-use List::Util ();
+use List::Util v1.33 ();
 use Scalar::Util qw(blessed);
 use Class::Method::Modifiers qw(install_modifier);
 
